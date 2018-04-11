fdfef8f7e87957f0ea06b59d95c092a0484e5c5b
DianaGD <37438077+DianaGD@users.noreply.github.com>
2018-04-11 19:20:03 +0200

    Update ConstantesSQL.java

--- proyectoGAG/src/constantesSQL/ConstantesSQL.java
+++ proyectoGAG/src/constantesSQL/ConstantesSQL.java
@@ -1,6 +1,6 @@
 package constantesSQL;
 
 public class ConstantesSQL {
-	public static final String INSERCION_ADMINISTRADOR = "insert into tabla_administrador (login, pass) values (?,?)";
-	public static final String IDENTIFICACION_ADMIN = "select id from tabla_administrador where login = ? and pass = ?";
+	public static final String IDENTIFICACION_ADMINISTRADOR = "select id from tabla_admin where login = ? and pass = ?";
+	public static final String IDENTIFICACION_EMPLEADO = "select id from tabla_empleado where login = ? and password = ?";
 }
