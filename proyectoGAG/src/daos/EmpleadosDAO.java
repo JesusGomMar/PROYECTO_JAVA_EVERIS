e127da081ef8a785ceba75b4b04bd6f9ed9584c1
Carlos <37349283+caresdo@users.noreply.github.com>
2018-04-13 12:19:23 +0200

    Update EmpleadosDAO.java

--- proyectoGAG/src/daos/EmpleadosDAO.java
+++ proyectoGAG/src/daos/EmpleadosDAO.java
@@ -4,6 +4,6 @@ import modelo.Empleado;
 
 public interface EmpleadosDAO {
 	boolean identificarEmpleado(String usuario, String pass);
-
 	void registrarEmpleado(Empleado empleado);
+	List<Empleado>obtenerEmpleados(List<String> valueDeCadaSelect);	
 }
