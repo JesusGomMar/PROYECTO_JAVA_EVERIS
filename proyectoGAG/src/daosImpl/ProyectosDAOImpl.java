c743a4aa911685ce124b7578c2953cb83521aacb
Carlos <37349283+caresdo@users.noreply.github.com>
2018-04-16 16:21:54 +0200

    Update ProyectosDAOImpl.java

--- proyectoGAG/src/daosImpl/ProyectosDAOImpl.java
+++ proyectoGAG/src/daosImpl/ProyectosDAOImpl.java
@@ -15,7 +15,7 @@ public class ProyectosDAOImpl extends GenericDAO implements ProyectosDAO{
 		try {
 			PreparedStatement ps = miConexion
 					.prepareStatement(ConstantesSQL.INSERCION_PROYECTO);
-			ps.setString(1, proyecto.getNombre());
+			ps.setString(1, proyecto.getProyecto());
 			ps.setString(2, proyecto.getComentario());
 			ps.execute();
 			ps.close();
@@ -25,4 +25,31 @@ public class ProyectosDAOImpl extends GenericDAO implements ProyectosDAO{
 		}
 		desconectar();
 	}
+	
+		@Override
+	public List<Proyecto> obtenerProyectos() {
+		conectar();
+		List<Proyecto> proyectos = new ArrayList<Proyecto>();
+		
+		PreparedStatement ps;
+		try {
+			ps = miConexion.prepareStatement(ConstantesSQL.SELECCION_PROYECTOS);
+			ResultSet resultado =ps.executeQuery();
+			
+			while(resultado.next()){
+				Proyecto proyecto = new Proyecto();
+				proyecto.setProyecto(resultado.getString("proyecto"));
+				proyecto.setComentario(resultado.getString("comentario"));
+				proyecto.setId(resultado.getInt("id"));
+				proyectos.add(proyecto);
+				}
+			} catch (SQLException e) {
+			System.out.println("error sql listar");
+			System.out.println(e.getMessage());
+		}
+		
+		desconectar();
+		return proyectos;
+	}//end obtener
+	
 }
