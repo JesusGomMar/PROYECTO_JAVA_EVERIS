8724040f8f31bef49fc41a80dd2aab8e124d428c
DianaGD <37438077+DianaGD@users.noreply.github.com>
2018-04-18 09:53:36 +0200

    Update ServletEditarEmpleado.java

--- proyectoGAG/src/servlets/ServletEditarEmpleado.java
+++ proyectoGAG/src/servlets/ServletEditarEmpleado.java
@@ -15,11 +15,24 @@ import daosImpl.EmpleadosDAOImpl;
 @WebServlet("/ServletEditarEmpleado")
 public class ServletEditarEmpleado extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-		protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int id = Integer.parseInt(request.getParameter("id"));
-		EmpleadosDAO empleadosDAO = new EmpleadosDAOImpl();
-		Empleado empleadoAEditar = empleadosDAO.obtenerEmpleadoPorId(id);
-		request.setAttribute("empleadoAEditar", empleadoAEditar);
-		request.getRequestDispatcher("editarEmpleado.jsp").forward(request, response);
-	}
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	int id = Integer.parseInt(request.getParameter("id"));
+	EmpleadosDAO empleadosDAO = new EmpleadosDAOImpl();
+	Empleado empleadoAEditar = empleadosDAO.obtenerEmpleadoPorId(id);
+	List<Competencia> disponibilidades = empleadosDAO.obtenerDisponibilidades();
+	List<Competencia> competencias = empleadosDAO.obtenerCompetencias();
+	List<Competencia> conocimientos = empleadosDAO.obtenerConocimientos();
+	List<Competencia> disponibilidad = empleadosDAO.obtenerDisponibilidades(id);
+	List<Competencia> competencia = empleadosDAO.obtenerCompetencias(id);
+	List<Competencia> conocimiento = empleadosDAO.obtenerConocimientos(id);
+	request.setAttribute("disponibilidad", disponibilidad);
+	request.setAttribute("competencia", competencia);
+	request.setAttribute("conocimiento", conocimiento);
+	System.out.println("conocimiento="+conocimiento);
+	request.setAttribute("disponibilidades", disponibilidades);
+	request.setAttribute("competencias", competencias);
+	request.setAttribute("conocimientos", conocimientos);
+	System.out.println("conocimientos="+conocimientos);
+	request.setAttribute("empleadoAEditar", empleadoAEditar);
+	request.getRequestDispatcher("editarEmpleado.jsp").forward(request, response);
 }
